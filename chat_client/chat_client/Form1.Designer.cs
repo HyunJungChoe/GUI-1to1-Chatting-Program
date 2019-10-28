08c8db5e5dba29040dcc02647bc202e4d8e75dd6
HyunJung Choe <46165574+HyunJungChoe@users.noreply.github.com>
2019-10-28 12:07:46 +0900

    Add files via upload

---
+++ chat_client/chat_client/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace chat_client
+{
+    partial class ChatForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.txtHistory = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTTS = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(240)))), ((int)(((byte)(255)))));
+            this.label3.Font = new System.Drawing.Font("맑은 고딕", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 20);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "서버 주소";
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtAddress.Location = new System.Drawing.Point(92, 6);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(144, 27);
+            this.txtAddress.TabIndex = 19;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(240)))), ((int)(((byte)(255)))));
+            this.label1.Font = new System.Drawing.Font("맑은 고딕", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(261, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 20);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "ID";
+            // 
+            // txtID
+            // 
+            this.txtID.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtID.Location = new System.Drawing.Point(291, 5);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(144, 27);
+            this.txtID.TabIndex = 21;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(655, 14);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 22;
+            this.btnStart.Text = "시작";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(462, 6);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(110, 28);
+            this.btnConnect.TabIndex = 23;
+            this.btnConnect.Text = "연결";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.OnConnectToServer);
+            // 
+            // txtHistory
+            // 
+            this.txtHistory.Location = new System.Drawing.Point(12, 45);
+            this.txtHistory.Name = "txtHistory";
+            this.txtHistory.ReadOnly = true;
+            this.txtHistory.Size = new System.Drawing.Size(560, 350);
+            this.txtHistory.TabIndex = 24;
+            this.txtHistory.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(240)))), ((int)(((byte)(255)))));
+            this.label2.Font = new System.Drawing.Font("맑은 고딕", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(12, 409);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 20);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "보낼 텍스트";
+            // 
+            // txtTTS
+            // 
+            this.txtTTS.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtTTS.Location = new System.Drawing.Point(107, 402);
+            this.txtTTS.Name = "txtTTS";
+            this.txtTTS.Size = new System.Drawing.Size(349, 27);
+            this.txtTTS.TabIndex = 19;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(462, 401);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(110, 28);
+            this.btnSend.TabIndex = 23;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.OnSendData);
+            // 
+            // ChatForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 441);
+            this.Controls.Add(this.txtHistory);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTTS);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Font = new System.Drawing.Font("맑은 고딕", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "ChatForm";
+            this.Text = "Multi Chat Client";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ChatForm_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.RichTextBox txtHistory;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTTS;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
---
+++ chat_server/chat_server/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace chat_server
+{
+    partial class ChatForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtHistory = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTTS = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(240)))), ((int)(((byte)(255)))));
+            this.label3.Font = new System.Drawing.Font("맑은 고딕", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.Location = new System.Drawing.Point(12, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 20);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "서버 주소";
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtAddress.Location = new System.Drawing.Point(92, 17);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(144, 21);
+            this.txtAddress.TabIndex = 18;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(240)))), ((int)(((byte)(255)))));
+            this.label1.Font = new System.Drawing.Font("맑은 고딕", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(258, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 20);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "포트 번호";
+            // 
+            // txtPort
+            // 
+            this.txtPort.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtPort.Location = new System.Drawing.Point(338, 19);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(144, 21);
+            this.txtPort.TabIndex = 18;
+            this.txtPort.Text = "15000";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(497, 17);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 19;
+            this.btnStart.Text = "시작";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BeginStartServer);
+            // 
+            // txtHistory
+            // 
+            this.txtHistory.Location = new System.Drawing.Point(12, 43);
+            this.txtHistory.Name = "txtHistory";
+            this.txtHistory.ReadOnly = true;
+            this.txtHistory.Size = new System.Drawing.Size(560, 350);
+            this.txtHistory.TabIndex = 21;
+            this.txtHistory.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(240)))), ((int)(((byte)(255)))));
+            this.label2.Font = new System.Drawing.Font("맑은 고딕", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(12, 408);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 20);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "보낼 텍스트";
+            // 
+            // txtTTS
+            // 
+            this.txtTTS.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtTTS.Location = new System.Drawing.Point(102, 408);
+            this.txtTTS.Name = "txtTTS";
+            this.txtTTS.Size = new System.Drawing.Size(379, 21);
+            this.txtTTS.TabIndex = 18;
+            // 
+            // textBox2
+            // 
+            this.textBox2.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textBox2.Location = new System.Drawing.Point(92, 797);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(144, 21);
+            this.textBox2.TabIndex = 18;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(487, 406);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 19;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.OnSendData);
+            // 
+            // ChatForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 441);
+            this.Controls.Add(this.txtHistory);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtTTS);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Name = "ChatForm";
+            this.Text = "Multi Chat Server";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ChatForm_Closing);
+            this.Load += new System.EventHandler(this.OnFromLoaded);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.RichTextBox txtHistory;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTTS;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
